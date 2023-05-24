68070064d06b9ca852b5d11fdd14a5d061a70a41
Blake Rutledge <blake@blakerutledge.com>
2023-05-24 10:05:47 -0700

    minimal progress sorting out scope and organization

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@
 
 use winit::{
     event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
-    window::WindowBuilder,
+    event_loop::{ControlFlow, EventLoop, EventLoopBuilder, EventLoopWindowTarget},
+    window::{Window, WindowBuilder},
 };
 
 use eframe::egui;
@@ -20,60 +20,122 @@ use tray_icon::{
 
 struct WatchdogApp {
     tray: TrayIcon,
+    // menu: Box<Menu>,
     event_loop: EventLoop<()>,
-    // gui
-    // other state ?
+    event_task: Box<dyn FnMut(Event<'_, ()>, &EventLoopWindowTarget<()>, &mut ControlFlow)>,
+    window: Window,
+    // window_loop: EventLoop<()>,
+    // other state ...
 }
 
 impl WatchdogApp {
-    fn new() -> WatchdogApp {
+    fn build() -> WatchdogApp {
+        //
+        // - - - SYSTEM TRAY
+        //
+        // menu
+        let menu = Menu::new();
+        let menu_item_open = MenuItem::new("Open Window", true, None);
+        let menu_item_exit = MenuItem::new("Exit App", true, None);
+        menu.append(&menu_item_open);
+        menu.append(&menu_item_exit);
+        // icon
+        let path = std::path::Path::new("./assets/images/watchdog-logo.png");
+        dbg!(path);
+        let icon = load_icon(path);
+        // tray entity
+        let tray = TrayIconBuilder::new()
+            .with_menu(Box::new(menu))
+            .with_tooltip("Watchdog")
+            .with_icon(icon)
+            .build()
+            .unwrap();
+
+        //
+        // - - - EVENT LOOP
+        //
+        let event_loop = EventLoopBuilder::with_user_event().build();
+
+        //
+        // - - - GUI WINDOW
+        //
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+        // FnMut(Event<'_, T>, &EventLoopWindowTarget<T>, &mut ControlFlow),
+        // F: 'static + FnMut(Event<'_, T>, &EventLoopWindowTarget<T>, &mut ControlFlow),
+        let event_task = Box::new(
+            move |event: Event<'_, ()>,
+                  _: &EventLoopWindowTarget<()>,
+                  mut control_flow: &mut ControlFlow| {
+                *control_flow = ControlFlow::Wait;
+
+                if let Ok(event) = MenuEvent::receiver().try_recv() {
+                    // TO DO need scope fixed like everywhere ?
+
+                    //     if event.id == menu_item_open.id() {
+                    //         println!("open")s
+                    //     } else if event.id == menu_item_exit.id() {
+                    //         println!("close")
+                    //     }
+                    // }
+                    println!("{:?}", event.id);
+                }
+
+                // exit
+                match event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        window_id,
+                    } => *control_flow = ControlFlow::Exit,
+                    _ => (),
+                }
+            },
+        );
+
         WatchdogApp {
-            tray: init_tray(),
-            event_loop: EventLoop::new(),
+            tray,
+            // menu,
+            event_loop,
+            event_task,
+            window,
         }
     }
 }
 
 fn main() {
-    let a = WatchdogApp::new();
-    a.event_loop.run(move |event, _, control_flow| {
+    let app = WatchdogApp::build();
+
+    app.event_loop.run(app.event_task);
+
+    //     *control_flow = ControlFlow::Wait;
+
+    //     if let Ok(event) = MenuEvent::receiver().try_recv() {
+    //         // TO DO need scope fixed like everywhere ?
+
+    //         //     if event.id == menu_item_open.id() {
+    //         //         println!("open")s
+    //         //     } else if event.id == menu_item_exit.id() {
+    //         //         println!("close")
+    //         //     }
+    //         // }
+    //         println!("{:?}", event.id);
+    //     }
+    // });
+
+    /*
+    app.window_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        if let Ok(event) = MenuEvent::receiver().try_recv() {
-            // TO DO need scope fixed like everywhere ?
-
-            //     if event.id == menu_item_open.id() {
-            //         println!("open")s
-            //     } else if event.id == menu_item_exit.id() {
-            //         println!("close")
-            //     }
-            // }
-            println!("{:?}", event.id);
+        // exit
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
         }
     });
-}
-
-fn init_tray() -> TrayIcon {
-    // TRAY ICON
-    let menu = Menu::new();
-    let menu_item_open = MenuItem::new("Open Window", true, None);
-    let menu_item_exit = MenuItem::new("Exit App", true, None);
-
-    menu.append(&menu_item_open);
-    menu.append(&menu_item_exit);
-
-    let path = std::path::Path::new("./assets/images/watchdog-logo.png");
-    dbg!(path);
-    let icon = load_icon(path);
-
-    let t = TrayIconBuilder::new()
-        .with_menu(Box::new(menu))
-        .with_tooltip("Watchdog")
-        .with_icon(icon)
-        .build()
-        .unwrap();
-
-    t
+    */
 }
 
 // fn init_loop() -> EventLoop<()> {
