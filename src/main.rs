aae2d7dd1095113c8639acf55fbf01864efcaaa4
Blake Rutledge <blake@blakerutledge.com>
2023-05-25 00:05:09 -0700

    smol improvementz

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,76 @@
 
 use winit::{
     event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop, EventLoopBuilder, EventLoopWindowTarget},
-    window::{Window, WindowBuilder},
+    event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
+    window::WindowBuilder,
 };
 
-use eframe::egui;
+// use eframe::egui;
 
 use tray_icon::{
-    menu::{Menu, MenuEvent, MenuItem, PredefinedMenuItem, Submenu},
-    TrayEvent, TrayIcon, TrayIconBuilder,
+    menu::{Menu, MenuEvent, MenuItem}, //, MenuItemExt, PredefinedMenuItem, Submenu},
+    // TrayEvent,
+    TrayIcon,
+    TrayIconBuilder,
 };
 
 // - - - MAIN - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 struct WatchdogApp {
     tray: TrayIcon,
-    // menu: Box<Menu>,
-    event_loop: EventLoop<()>,
-    event_task: Box<dyn FnMut(Event<'_, ()>, &EventLoopWindowTarget<()>, &mut ControlFlow)>,
-    window: Window,
-    // window_loop: EventLoop<()>,
+    menu_elements: Vec<MenuElement>,
+    event_loop: Option<EventLoop<()>>,
+    window: winit::window::Window,
     // other state ...
 }
 
+struct MenuElement {
+    id: u32,
+    item: MenuItem,
+    handler: fn(),
+}
+
+impl MenuElement {
+    fn new(text: &str, active: bool, handler: fn()) -> Self {
+        let item = MenuItem::new(text, active, None);
+        let id = item.id();
+        MenuElement { id, item, handler }
+    }
+}
+
+fn open_window() {
+    println!("TODO: Open Window");
+}
+
+fn close_window() {
+    println!("TODO: Close Window");
+}
+
 impl WatchdogApp {
-    fn build() -> WatchdogApp {
+    fn new() -> WatchdogApp {
         //
         // - - - SYSTEM TRAY
         //
         // menu
-        let menu = Menu::new();
-        let menu_item_open = MenuItem::new("Open Window", true, None);
-        let menu_item_exit = MenuItem::new("Exit App", true, None);
-        menu.append(&menu_item_open);
-        menu.append(&menu_item_exit);
+        let menu = Box::new(Menu::new());
+        let mut menu_elements: Vec<MenuElement> = Vec::new();
+
+        let open = MenuElement::new("Open Window", true, open_window);
+        let close = MenuElement::new("Close Window", true, close_window);
+
+        menu.append(&open.item.clone());
+        menu.append(&close.item.clone());
+
+        menu_elements.push(open);
+        menu_elements.push(close);
+
         // icon
         let path = std::path::Path::new("./assets/images/watchdog-logo.png");
         dbg!(path);
         let icon = load_icon(path);
         // tray entity
         let tray = TrayIconBuilder::new()
-            .with_menu(Box::new(menu))
+            .with_menu(menu)
             .with_tooltip("Watchdog")
             .with_icon(icon)
             .build()
@@ -61,51 +90,46 @@ impl WatchdogApp {
         //
         let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-        // FnMut(Event<'_, T>, &EventLoopWindowTarget<T>, &mut ControlFlow),
-        // F: 'static + FnMut(Event<'_, T>, &EventLoopWindowTarget<T>, &mut ControlFlow),
-        let event_task = Box::new(
-            move |event: Event<'_, ()>,
-                  _: &EventLoopWindowTarget<()>,
-                  mut control_flow: &mut ControlFlow| {
-                *control_flow = ControlFlow::Wait;
-
-                if let Ok(event) = MenuEvent::receiver().try_recv() {
-                    // TO DO need scope fixed like everywhere ?
-
-                    //     if event.id == menu_item_open.id() {
-                    //         println!("open")s
-                    //     } else if event.id == menu_item_exit.id() {
-                    //         println!("close")
-                    //     }
-                    // }
-                    println!("{:?}", event.id);
-                }
-
-                // exit
-                match event {
-                    Event::WindowEvent {
-                        event: WindowEvent::CloseRequested,
-                        window_id,
-                    } => *control_flow = ControlFlow::Exit,
-                    _ => (),
-                }
-            },
-        );
-
         WatchdogApp {
             tray,
-            // menu,
-            event_loop,
-            event_task,
+            menu_elements,
+            event_loop: Some(event_loop),
             window,
         }
     }
+
+    fn update<T>(&self, event: Event<'_, T>, control_flow: &mut ControlFlow) {
+        *control_flow = ControlFlow::Poll;
+
+        if let Ok(event) = MenuEvent::receiver().try_recv() {
+            for i in &self.menu_elements {
+                if i.id == event.id {
+                    (i.handler)();
+                }
+            }
+        }
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        }
+    }
+
+    fn close_window() {}
 }
 
 fn main() {
-    let app = WatchdogApp::build();
+    let mut app = WatchdogApp::new();
 
-    app.event_loop.run(app.event_task);
+    app.event_loop
+        .take()
+        .unwrap()
+        .run(move |event, _, control_flow| app.update(event, control_flow))
 
     //     *control_flow = ControlFlow::Wait;
 
@@ -171,22 +195,22 @@ event_loop.run(move |event, _, control_flow| {
 });
 */
 
-fn open_window() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+// fn open_window() {
+//     let event_loop = EventLoop::new();
+//     let window = WindowBuilder::new().build(&event_loop).unwrap();
+//     event_loop.run(move |event, _, control_flow| {
+//         *control_flow = ControlFlow::Wait;
 
-        // exit
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
-}
+//         // exit
+//         match event {
+//             Event::WindowEvent {
+//                 event: WindowEvent::CloseRequested,
+//                 window_id,
+//             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+//             _ => (),
+//         }
+//     });
+// }
 
 fn load_icon(path: &std::path::Path) -> tray_icon::icon::Icon {
     let (icon_rgba, icon_width, icon_height) = {
@@ -201,25 +225,25 @@ fn load_icon(path: &std::path::Path) -> tray_icon::icon::Icon {
         .expect("Failed to open icon")
 }
 
-fn init_gui() -> eframe::Result<()> {
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
+// fn init_gui() -> eframe::Result<()> {
+//     // Our application state:
+//     let mut name = "Arthur".to_owned();
+//     let mut age = 42;
 
-    let options = eframe::NativeOptions::default();
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
-        });
-    })
-}
+//     let options = eframe::NativeOptions::default();
+//     eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+//         egui::CentralPanel::default().show(ctx, |ui| {
+//             ui.heading("My egui Application");
+//             ui.horizontal(|ui| {
+//                 let name_label = ui.label("Your name: ");
+//                 ui.text_edit_singleline(&mut name)
+//                     .labelled_by(name_label.id);
+//             });
+//             ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+//             if ui.button("Click each year").clicked() {
+//                 age += 1;
+//             }
+//             ui.label(format!("Hello '{name}', age {age}"));
+//         });
+//     })
+// }
